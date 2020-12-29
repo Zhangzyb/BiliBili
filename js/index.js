fe1521bb1e30316880850d22feb7b4bc851d4419
zyb <841733577@qq.com>
2020-12-29 16:11:18 +0800

    动态下拉完成

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
 let lis = document.querySelectorAll('.dropdown')
+let tabcate = document.querySelectorAll('.tab-cate div')
 console.log(lis)
+
 for (let i = 0; i < lis.length; i++){
     lis[i].onmouseover = function () {
-        this.children[1].style.display = "block"
+        this.children[1].style.display = "block";
     }
     lis[i].onmouseout = function () {
-        this.children[1].style.display = "none"
+        this.children[1].style.display = "none";
     }
 }
 
-
+for(let i = 0;i < tabcate.length;i++){
+    tabcate[i].onclick = function () {
+        for(let i = 0;i < tabcate.length;i++){
+            tabcate[i].className = '';
+        }
+        tabcate[i].className = 'active';
+    }
+}
