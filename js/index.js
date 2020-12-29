6656f22feac06ff65a2bdad56a1e8be2f314dd8c
zyb <841733577@qq.com>
2020-12-29 22:22:11 +0800

    收藏下拉完成

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 let lis = document.querySelectorAll('.dropdown')
 let tabcate = document.querySelectorAll('.tab-cate div')
-console.log(lis)
+let tab_list = document.querySelectorAll('.tab-list li')
+let items = document.querySelectorAll('.item')
+
+console.log(tab_list)
 
 for (let i = 0; i < lis.length; i++){
     lis[i].onmouseover = function () {
@@ -17,5 +20,18 @@ for(let i = 0;i < tabcate.length;i++){
             tabcate[i].className = '';
         }
         tabcate[i].className = 'active';
+    }
+}
+
+for (let i = 0;i < tab_list.length;i++) {
+    tab_list[i].onclick = function() {
+        console.log(i)
+        for (let i = 0;i < tab_list.length;i++){
+            console.log(i)
+            tab_list[i].className = '';
+            items[i].style.display = 'none';
+        }
+        tab_list[i].className = 'current';
+        items[i].style.display = 'block';
     }
 }
