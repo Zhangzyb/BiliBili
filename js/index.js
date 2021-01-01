02a8d6cdba460482a90c15c52d54cd371a5f8722
zyb <841733577@qq.com>
2021-01-01 22:03:24 +0800

    调整html结构及命名

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 let lis = document.querySelectorAll('.dropdown')
-let tabcate = document.querySelectorAll('.tab-cate div')
-let tab_list = document.querySelectorAll('.tab-list li')
-let items = document.querySelectorAll('.item')
 
-console.log(tabcate)
+console.log(lis)
 
 for (let i = 0; i < lis.length; i++){
     lis[i].onmouseover = function () {
@@ -11,27 +8,5 @@ for (let i = 0; i < lis.length; i++){
     }
     lis[i].onmouseout = function () {
         this.children[1].style.display = "none";
-    }
-}
-
-for(let i = 0;i < tabcate.length;i++){
-    tabcate[i].onclick = function () {
-        for(let i = 0;i < tabcate.length;i++){
-            tabcate[i].className = '';
-        }
-        tabcate[i].className = 'active';
-    }
-}
-
-for (let i = 0;i < tab_list.length;i++) {
-    tab_list[i].onclick = function() {
-        console.log(i)
-        for (let i = 0;i < tab_list.length;i++){
-            console.log(i)
-            tab_list[i].className = '';
-            items[i].style.display = 'none';
-        }
-        tab_list[i].className = 'current';
-        items[i].style.display = 'block';
     }
 }
