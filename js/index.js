8d62cc5352f1d2a1a9d8fecd49a75dc0d651902a
zyb <841733577@qq.com>
2021-01-02 21:44:44 +0800

    导航栏重写完成

--- js/index.js
+++ js/index.js
@@ -1,12 +1,1 @@
-let lis = document.querySelectorAll('.dropdown')
 
-console.log(lis)
-
-for (let i = 0; i < lis.length; i++){
-    lis[i].onmouseover = function () {
-        this.children[1].style.display = "block";
-    }
-    lis[i].onmouseout = function () {
-        this.children[1].style.display = "none";
-    }
-}
