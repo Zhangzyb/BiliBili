0ccbbb6d740d6a09d4c44078cc61456b0cc52d13
zyb <841733577@qq.com>
2021-01-03 22:14:15 +0800

    菜单栏背景图片及导航栏左侧完成

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
+// 
+let lis = document.querySelectorAll('.tab-list ul li ')
+let items = document.querySelectorAll('.tab-con .tab-items > .item')
+let tabcate = document.querySelectorAll('.tab-cate div')
 
+// 收藏下拉选项卡
+for (let i = 0; i < lis.length; i++) {
+    lis[i].onclick = () => {
+        for (let i = 0; i < lis.length; i++) {
+            lis[i].className = '';
+            items[i].style.display = 'none';
+        }
+        lis[i].className = 'current';
+        items[i].style.display = 'block';
+    }
+}
+
+// 动态下拉选项卡和历史下拉选项卡通过点击触发样式的改变
+// 两部分没有区分，所以当点击了动态里的视频动态或专栏动态等选项后，历史选项卡的动态历史的样式会消失
+// 一个小Bug，懒得改了
+for(let i = 0;i < tabcate.length;i++){
+    tabcate[i].onclick = function () {
+        for(let i = 0;i < tabcate.length;i++){
+            tabcate[i].className = '';
+        }
+        tabcate[i].className = 'active';
+    }
+}
