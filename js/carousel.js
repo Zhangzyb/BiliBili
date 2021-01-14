6816d69435382db13b188cf653d3cbd0f1b83a13
zyb <841733577@qq.com>
2021-01-14 19:14:36 +0800

    使用类重新封装轮播图以完成复用

--- js/carousel.js
+++ js/carousel.js
@@ -9,12 +9,29 @@
 //    <div class="btn"></div>  
 // </div>
 
-let Carousel = (focusObj) => {
-    const focusObjWidth = focusObj.offsetWidth;
-    let carouselObj = focusObj.children[0];
-    let carouselBtn = focusObj.children[1];
+// 点击时给相应元素加上选中样式，并取消其他元素的选中样式。
+
+
+class CarouselObj {
+    constructor(Elenobj) {
+        this.focusObjWidth = Elenobj.offsetWidth; // 轮播区域宽度
+        this.carouselObj = Elenobj.children[0];   // 包含所有轮播图的盒子
+        this.carouselBtn = Elenobj.children[1];   // 小圆点
+        this.imgIndex = 0;  // 分别用于控制图片和圆点显示
+        this.btnIndex = 0;
+        this.gloablTimer = null; //自动轮播的定时器
+    }
+
+    // 点击时给相应元素加上选中样式，并取消其他元素的选中样式。
+    changeClass(obj, index, cur) {
+        for (let i = 0; i < obj.children.length; i++) {
+            obj.children[i].className = '';
+        }
+        obj.children[index].className = cur;
+    }
+
     //封装动画，参数：使用动画的对象、移动的距离、回调函数(可选)。
-    let animate = (obj, target, callback) => {
+    animate(obj, target, callback) {
         clearInterval(obj.timer);
         obj.timer = setInterval(() => {
             let step = (target - obj.offsetLeft) / 10;
@@ -27,57 +44,45 @@ let Carousel = (focusObj) => {
         }, 15)
     }
 
-    // 点击时给相应元素加上选中样式，并取消其他元素的选中样式。
-    let changeClass = (obj, index, cur) => {
-        for (let i = 0; i < obj.children.length; i++) {
-            obj.children[i].className = '';
-        }
-        obj.children[index].className = cur;
-    }
-
-    // 分别用于控制图片和圆点显示
-    let imgIndex = btnIndex = 0;
-
-    // 根据图片数生成小圆点数，并为每个圆点绑定点击事件。
-    for (let i = 0; i < carouselObj.children.length; i++) {
-        let li = document.createElement('li');
-        carouselBtn.appendChild(li);
-        li.addEventListener('click', () => {
-            btnIndex = imgIndex = i;
-            changeClass(carouselBtn, i, 'cur');
-            animate(carouselObj, -i * focusObjWidth);
-        });
-    }
-    carouselBtn.children[0].className = 'cur';
-    // 复制第一张图片以实现无缝轮播
-    let copyChild = carouselObj.children[0].cloneNode(true);
-    carouselObj.appendChild(copyChild);
-
     // 自动轮播
-    let gloablTimer;
-    let autoChange = () => {
-        gloablTimer = setInterval(() => {
-            if (imgIndex === carouselObj.children.length - 1) {
-                carouselObj.style.left = 0;
-                imgIndex = 0;
+    autoChange() {
+        this.gloablTimer = setInterval(() => {
+            if (this.imgIndex === this.carouselObj.children.length - 1) {
+                this.carouselObj.style.left = 0;
+                this.imgIndex = 0;
             }
-            imgIndex++;
-            animate(carouselObj, -imgIndex * focusObjWidth);
-            btnIndex++;
-            if (btnIndex === carouselBtn.children.length) {
-                btnIndex = 0;
+            this.imgIndex++;
+            this.animate(this.carouselObj, - this.imgIndex * this.focusObjWidth);
+            this.btnIndex++;
+            if (this.btnIndex === this.carouselBtn.children.length) {
+                this.btnIndex = 0;
             }
-            changeClass(carouselBtn, btnIndex, 'cur');
+            this.changeClass(this.carouselBtn, this.btnIndex, 'cur');
         }, 5000)
     }
-    autoChange();
-    // 鼠标经过图片时移除定时器，离开时重启定时器
-    carouselObj.addEventListener('mouseover', () => {
-        clearInterval(gloablTimer);
-    })
-    carouselObj.addEventListener('mouseleave', () => {
-        autoChange();
-    })
+
+    carousel() {
+        // 根据图片数生成小圆点数，并为每个圆点绑定点击事件。
+        for (let i = 0; i < this.carouselObj.children.length; i++) {
+            let li = document.createElement('li');
+            this.carouselBtn.appendChild(li);
+            li.addEventListener('click', () => {
+                this.btnIndex = this.imgIndex = i;
+                this.changeClass(this.carouselBtn, i, 'cur');
+                this.animate(this.carouselObj, -i * this.focusObjWidth);
+            });
+        }
+        this.carouselBtn.children[0].className = 'cur';
+        // 复制第一张图片以实现无缝轮播
+        let copyChild = this.carouselObj.children[0].cloneNode(true);
+        this.carouselObj.appendChild(copyChild);
+        this.autoChange();
+        // 鼠标经过图片时移除定时器，离开时重启定时器
+        this.carouselObj.addEventListener('mouseover', () => {
+            clearInterval(this.gloablTimer);
+        })
+        this.carouselObj.addEventListener('mouseleave', () => {
+            this.autoChange();
+        })
+    }
 }
-
-
